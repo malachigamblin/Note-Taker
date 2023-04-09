d09061bc1a7f7f83c8b8d6647f249a70ff5a52dd
malachigamblin <malachi.gamblin@gmail.com>
2023-04-09 01:23:19 -0400

    added function to add a new note

--- server.js
+++ server.js
@@ -24,6 +24,25 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+function addNewThought(body, thoughtArray) {
+    const newThought = body;
+    if (!Array.isArray(thoughtArray))
+        thoughtArray = [];
+    
+    if (thoughtArray.length === 0)
+        thoughtArray.push(0);
+
+    body.id = thoughtArray[0];
+    thoughtArray[0]++;
+
+    thoughtArray.push(newThought);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(thoughtArray, null, 2)
+    );
+    return newThought;
+}
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
