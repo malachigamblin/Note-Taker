e1cdee352f82b425dcad8c30730a415147f320aa
malachigamblin <malachi.gamblin@gmail.com>
2023-04-09 10:03:31 -0400

    added ability to delete notes

--- server.js
+++ server.js
@@ -1,53 +1,72 @@
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const thoughts = require('./db/db.json');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const thoughts = require("./db/db.json");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/api/notes', (req, res) => {
-    res.json(thoughts.slice(1));
+app.get("/api/notes", (req, res) => {
+  res.json(thoughts.slice(1));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 function addNewThought(body, thoughtArray) {
-    const newThought = body;
-    if (!Array.isArray(thoughtArray))
-        thoughtArray = [];
-    
-    if (thoughtArray.length === 0)
-        thoughtArray.push(0);
+  const newThought = body;
+  if (!Array.isArray(thoughtArray)) thoughtArray = [];
 
-    body.id = thoughtArray[0];
-    thoughtArray[0]++;
+  if (thoughtArray.length === 0) thoughtArray.push(0);
 
-    thoughtArray.push(newThought);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(thoughtArray, null, 2)
-    );
-    return newThought;
+  body.id = thoughtArray[0];
+  thoughtArray[0]++;
+
+  thoughtArray.push(newThought);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(thoughtArray, null, 2)
+  );
+  return newThought;
 }
 
-app.post('/api/notes', (req, res) => {
-    const newThought = addNewThought(req.body, thoughts);
-    res.json(newThought);
+app.post("/api/notes", (req, res) => {
+  const newThought = addNewThought(req.body, thoughts);
+  res.json(newThought);
+});
+
+function deleteThought(id, thoughtArray) {
+  for (let i = 0; i < thoughtArray.length; i++) {
+    let note = thoughtArray[i];
+
+    if (note.id == id) {
+      thoughtArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(thoughtArray, null, 2)
+      );
+
+      break;
+    }
+  }
+}
+
+app.delete("/api/notes/:id", (req, res) => {
+  deleteThought(req.params.id, thoughts);
+  res.json(true);
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
