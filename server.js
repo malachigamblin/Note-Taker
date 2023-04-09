092c8d43ae9b37ffdc94ae0f19229059331ac1a5
malachigamblin <malachi.gamblin@gmail.com>
2023-04-09 01:14:38 -0400

    added route for server and html to connect, pulling up notes

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const thoughts = require('./db/db.json');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(thoughts.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
